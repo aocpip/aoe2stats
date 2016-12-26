ead4b9538dd1ca3abcd21722f84f692dc609d2a3
pip <aocpip@gmail.com>
2016-12-26 16:33:37 +0100

    update changed techtrees

--- web/js/main.js
+++ web/js/main.js
@@ -177,8 +177,8 @@ function formatCiv(d, columns) {
 
 	var version = Cookies.get("aocversion");
 	if(d.tt.length > 0) {
-		content += "<div style='min-height: 240px;height: 100%;'>";
-		content += '<iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="images/techtrees/'+version+'/'+d.tt+'.html"></iframe>';
+		content += "<div style='min-height: 320px;height: 100%;'>";
+		content += '<iframe width="100%" height="100%" style="min-height: 320px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="images/techtrees/'+version+'/'+d.tt+'.html"></iframe>';
 		content += "</div>";
 	}
 
