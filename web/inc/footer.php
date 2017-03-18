2c0210aa0474cc9f2726f09878156f44b2e2b0ae
szist <szentandrasii@gmail.com>
2017-03-18 08:56:43 +0100

    Add HSZemi to footer

--- web/inc/footer.php
+++ web/inc/footer.php
@@ -13,7 +13,7 @@
     </div>
 
     <div style="text-align: center" class="light-glow">
-        Created by <a href="http://blog.aocpip.eu">pip</a> (2015-2017) Contributors: Jineapple
+        Created by <a href="http://blog.aocpip.eu">pip</a> (2015-2017) Contributors: Jineapple, HSZemi
     <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
         <input type="hidden" name="cmd" value="_s-xclick">
         <input type="hidden" name="hosted_button_id" value="X63U6AWPRDRJ8">
